54b840062b7d2406834bb49eb4248978ec280571
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2026-02-26 13:16:48 +0530

    Blade & Crown barber website

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ async function sendEmail(to, subject, html) {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST || 'smtp.gmail.com',
       port: parseInt(process.env.EMAIL_PORT) || 587,
-      secure: false,
+      secure: process.env.EMAIL_SECURE === 'true',
       auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
     });
     await transporter.sendMail({ from: process.env.EMAIL_FROM || process.env.EMAIL_USER, to, subject, html });
